01eed746dd467869ffc263b4e80cfe09fb50191b
Thomas Meyer <strayfatty@gmail.com>
2021-05-22 21:11:02 +0200

    add ui project

---
+++ src/Tand.UI/webpack.prod.js
@@ -0,0 +1,13 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const path = require('path');
+
+module.exports = merge(common, {
+    mode: 'production',
+    optimization: {
+        minimize: true
+    },
+    output: {
+        path: path.resolve(__dirname, '../Tand.Web/wwwroot')
+    }
+});
